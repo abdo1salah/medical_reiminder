eac6f1aefdaeade697fcf465ec4f293c7955183e
abdo1salah <100434550+abdo1salah@users.noreply.github.com>
2025-03-16 21:38:38 +0200

    email verification

--- app/src/main/java/com/example/medicalreiminder/viewModels/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/medicalreiminder/viewModels/AuthenticationViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.medicalreiminder.viewModels
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import com.example.medicalreiminder.model.UserModel
 import com.google.firebase.auth.ktx.auth
@@ -9,10 +12,11 @@ import com.google.firebase.ktx.Firebase
 class AuthenticationViewModel : ViewModel() {
     val auth = Firebase.auth
     val firestore = Firebase.firestore
-    fun signUp(
+    private fun signUp(
         email: String,
         password: String,
         name: String,
+        context: Context,
         onResult: (Boolean, String?) -> Unit
     ) {
         auth.createUserWithEmailAndPassword(email, password)
@@ -20,10 +24,12 @@ class AuthenticationViewModel : ViewModel() {
                 if (Result.isSuccessful) {
                     val userId = Result.result.user?.uid
                     val userModel = UserModel(userId!!, name, email)
-                    firestore.collection("users").document(userId)
+                    firestore.collection("users")
+                        .document(userId)
                         .set(userModel)
                         .addOnCompleteListener { dbResult ->
                             if (dbResult.isSuccessful) {
+                                verifyEmail(context)
                                 onResult(true, null)
                             } else {
                                 onResult(false, "something went wrong")
@@ -38,13 +44,34 @@ class AuthenticationViewModel : ViewModel() {
             }
     }
 
-    fun login(email: String, password: String, onResult: (Boolean, String?) -> Unit) {
+    private fun login(
+        email: String,
+        password: String,
+        context: Context,
+        onResult: (Boolean, String?) -> Unit
+    ) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
-                    onResult(true,null)
+                    if (auth.currentUser!!.isEmailVerified) {
+                        onResult(true, null)
+                    } else {
+                        Toast.makeText(context, "verify your email first", Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 } else {
-                    onResult(false,it.exception?.localizedMessage)
+                    onResult(false, it.exception?.localizedMessage)
+                }
+            }
+    }
+
+    private fun verifyEmail(context: Context) {
+        auth.currentUser!!.sendEmailVerification()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "check your email", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "something went wrong", Toast.LENGTH_SHORT).show()
                 }
             }
     }
